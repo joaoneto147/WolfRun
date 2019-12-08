15dba86b8f64224be003635e8c25b41f800cb732
unknown <by.srneto@gmail.com>
2019-12-08 04:04:39 -0300

    Iniciando versionamento do projeto

---
+++ Assets/2D Free Platformer Char/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Mover();
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Mover();
+        }
+    }
+
+    private void Mover()
+    {
+
+    }
+}
